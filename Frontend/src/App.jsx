01314f3556c26db70aa62567ca9fb30e93696e64
karmaboy1309 <darshanmawana1305@gmail.com>
2026-01-28 19:06:40 +0530

    #

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import JobList from "./pages/JobList";
 import CreateJob from "./pages/CreateJob";   // 👉 NEW IMPORT
+import JobDetails from "./pages/JobDetails";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Home Job List */}
-        <Route path="/" element={<JobList />} />
-
-        {/* Create Job Page */}
-        <Route path="/create" element={<CreateJob />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* Welcome / Home */}
+          <Route path="/" element={<Welcome />} />
+          <Route path="/jobs" element={<JobList />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create" element={<CreateJob />} />
+          <Route path="/jobs/:id" element={<JobDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
