cf22db71844768f75e5ffac8de4ec0cc2e061d4c
Darshan Makwana <darshanmakwana1305@gmail.com>
2026-03-14 19:15:20 +0530

    #

--- Frontend/src/pages/CreateJob.jsx
+++ Frontend/src/pages/CreateJob.jsx
@@ -4,8 +4,6 @@ import { useNavigate } from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner";
 import ErrorMessage from "../components/ErrorMessage";
 
-
-
 const CreateJob = () => {
   const navigate = useNavigate();
 
